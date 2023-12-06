61a9b1f00bd30d89278b2e75b1fa42139772d43a
Prince Bansal <prince.bansal008@gmail.com>
2023-12-06 20:49:59 +0530

    first code done

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,12 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let max = numbers[0];
+    for (let i = 0; i < numbers.length; i++)
+        if (max < numbers[i]) max = numbers[i];
+
+    return max;
 }
+// console.log(findLargestElement([2, 6969, 5, 6, 89, 233]));
 
 module.exports = findLargestElement;
