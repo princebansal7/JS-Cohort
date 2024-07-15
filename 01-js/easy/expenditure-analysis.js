dd4ba3c26118f0e9dcc3177a28eebf5efda4ff68
Prince Bansal <prince.bansal008@gmail.com>
2024-07-16 01:08:25 +0530

    assignment-1 done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -11,10 +11,55 @@
 		itemName: 'Pizza',
 	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
+
+  const transactions = [
+    {
+        id: 1,
+        timestamp: 1656076800000,
+        price: 10,
+        category: "Food",
+        itemName: "Pizza",
+    },
+    {
+        id: 2,
+        timestamp: 1656259600000,
+        price: 20,
+        category: "Food",
+        itemName: "Burger",
+    },
+    {
+        id: 3,
+        timestamp: 1656019200000,
+        price: 15,
+        category: "Clothing",
+        itemName: "T-Shirt",
+    },
+    {
+        id: 4,
+        timestamp: 1656364800000,
+        price: 30,
+        category: "Electronics",
+        itemName: "Headphones",
+    },
+    {
+        id: 5,
+        timestamp: 1656105600000,
+        price: 25,
+        category: "Clothing",
+        itemName: "Jeans",
+    },
+];
+
+expected o/p: [{ category: 'Food', totalSpent: 30 }, { category: 'Clothing', totalSpent: 40 },{ category: 'Electronics', totalSpent: 30 }]
+    => do sum of same category and category should not repeat
 */
 
 function calculateTotalSpentByCategory(transactions) {
-    return [transactions.category, transactions.price];
+    const ans = [];
+    for (let i = 0; i < transactions.length; i++) {}
+    return ans;
 }
+
+console.log(calculateTotalSpentByCategory(transactions));
 
 module.exports = calculateTotalSpentByCategory;
